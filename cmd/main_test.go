4088ec3597705e6ceca03562e4420a3a00697419
kannapppan senthilnathan <55806176+KannappanSomu@users.noreply.github.com>
2023-01-20 16:01:31 +0100

    K (#3)
    * Update main.go
    
    * Update main_test.go

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -21,7 +21,7 @@ func Test_handler(t *testing.T) {
 	}{
 		{
 			"status OK",
-			args{"/", http.StatusOK, fmt.Sprintf("Helllo Americans from %s", host)},
+			args{"/", http.StatusOK, fmt.Sprintf("Helllo Americans1 from %s", host)},
 		},
 	}
 
